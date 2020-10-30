5135cbd69ed5a63bd692c28d7b7bb83047a6e2b1
kevinloffler <hi@kevinloeffler.ch>
2020-10-30 18:31:29 +0100

    Experimenting

--- Code/auhelper.py
+++ Code/auhelper.py
@@ -13,15 +13,6 @@ client = discord.Client()
 ### Set up:
 bot = commands.Bot(command_prefix='.')
 
-### Messages
-def commandsCMD(input):
-    if input == '.start':
-        pass
-    elif input == '.join':
-        pass
-    elif input == '.sus':
-        pass
-
 
 @bot.event
 async def on_ready():
@@ -29,12 +20,22 @@ async def on_ready():
     print("Bot " + bot.user.name + " connected to " + guild.name)
     print("With the ID: " + str(bot.user.id) + "\n")
 
+#@bot.command(name = 'run')
+#async def run(ctx):
+#    await ctx.send('running')
+
 @bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
+async def test(ctx):
+    await ctx.send('testing')
+
+@bot.command(name = 'admin')
+@commands.has_role('admin')
+async def admin(ctx):
+    await ctx.send('allowed')
 
 @bot.event
-async def on_message(message):
-    pass
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
 
 bot.run(TOKEN)
