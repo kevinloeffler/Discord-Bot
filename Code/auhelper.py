63f997bff83030877cca630086411e666d260eb0
kevinloeffler <hi@kevinloeffler.ch>
2020-10-31 15:48:59 +0100

    Removed member functions

--- Code/auhelper.py
+++ Code/auhelper.py
@@ -15,16 +15,6 @@ bot = commands.Bot(command_prefix = ".")
 async def on_ready():
     print("Bot is ready")
 
-# Join notification
-@bot.event
-async def on_member_join(member):
-    print("{m} has joined".format(m = member))
-
-# Left notification
-@bot.event
-async def on_member_remove(member):
-    print("{m} has left".format(m = member))
-
 # Command
 @bot.command()
 async def ping(ctx):
