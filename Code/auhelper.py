827d349349f85ee07c7093506170bed747d39ed9
kevinloffler <hi@kevinloeffler.ch>
2020-10-31 14:22:24 +0100

    Experimenting

--- Code/auhelper.py
+++ Code/auhelper.py
@@ -8,17 +8,34 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('GUILD_NAME')
 
-client = discord.Client()
 
 ### Set up:
+client = discord.Client()
 bot = commands.Bot(command_prefix='.')
+# guild = discord.utils.get(client.guilds)
+guild = client.guilds
+print(guild)
 
 
 @bot.event
 async def on_ready():
     guild = discord.utils.get(bot.guilds)
+    await guild.send("Connected")
     print("Bot " + bot.user.name + " connected to " + guild.name)
     print("With the ID: " + str(bot.user.id) + "\n")
+
+@bot.event
+async def on_disconnect():
+    await guild.send("Disconnected")
+    print("Bot " + bot.user.name + " disconnected from " + guild.name)
+
+@client.event
+async def on_message(message):
+    if message == "version":
+        myEmbed = discord.Embed(title = 'Current Version', description = "Version 1.0", color = "0x00ff00")
+        myEmbed.add_field(name = "Version Code:", value = "v1.0.0", inline = false)
+        myEmbed.set_footer(text = "Sample Footer")
+        await guild.send(embed = myEmbed)
 
 @bot.command()
 async def test(ctx):
@@ -34,4 +51,5 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the correct role for this command.')
 
-bot.run(TOKEN)
+client.run(TOKEN)
+# bot.run(TOKEN)
