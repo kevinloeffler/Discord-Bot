20d20c6a7e024c8c49ebad2234d9fc0368861742
kevinloeffler <hi@kevinloeffler.ch>
2020-10-31 17:08:12 +0100

    Extended on ready function

--- Code/auhelper.py
+++ Code/auhelper.py
@@ -13,9 +13,10 @@ bot = commands.Bot(command_prefix = ".")
 # On Connection
 @bot.event
 async def on_ready():
+    await bot.change_presence(status = discord.Status.online, activity = discord.Game("Among Us"))
     print("Bot is ready")
 
-# Command
+# Ping
 @bot.command()
 async def ping(ctx):
     await ctx.send(round((bot.latency * 1000), 2))
