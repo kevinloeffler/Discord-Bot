95e8afd57f728c44fad1782b5ececdfa191a367f
kevinloeffler <hi@kevinloeffler.ch>
2020-11-01 16:27:45 +0100

    Join / Leave Command Game Environment

--- Code/auhelper.py
+++ Code/auhelper.py
@@ -11,7 +11,6 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('GUILD_NAME')
 
 bot = commands.Bot(command_prefix = ".")
-admin = commands.Bot(command_prefix = "..")
 
 # On Connection
 @bot.event
@@ -47,8 +46,26 @@ async def about(ctx):
 ### Game Commands
 
 # Join
+@bot.command()
+async def join(ctx):
+    pid = str(ctx.author)
+    responseCode = activeGame.addPlayer(pid)
+    if responseCode == 0:
+        await ctx.send("No more player instances available.")
+    elif responseCode == -1:
+        await ctx.send("You have already joined the game.")
+    else:
+        await ctx.send("Joined as Player " + str(responseCode) + ".")
 
 # Leave
+@bot.command()
+async def leave(ctx):
+    pid = str(ctx.author)
+    responseCode = activeGame.removePlayer(pid)
+    if responseCode == 0:
+        await ctx.send("You have not joined a game.")
+    else:
+        await ctx.send("You left the game.")
 
 # Pick Color
 
@@ -61,6 +78,10 @@ async def about(ctx):
 
 # End Game
 
+# Game Info
+
+# Player Info
+
 # Change Player Color
 
 # Remove Player
@@ -69,7 +90,44 @@ async def about(ctx):
 
 ### Game Enviorenment
 
+class Player:
+    def __init__(self):
+        self.status = False
+        self.id = None
+        self.color = None
 
+    def pickColor(self, c):
+        self.color = c
+
+    def reset(self):
+        self.status = False
+        self.id = None
+        self.color = None
+
+class Game:
+    id = 111
+    # Create Empty Players
+    player1, player2, player3, player4, player5, player6, player7, player8, player9, player10 = Player(), Player(), Player(), Player(), Player(), Player(), Player(), Player(), Player(), Player()
+    players = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10]
+
+    def addPlayer(ctx, pid):
+        counter = 0
+        for p in Game.players:
+            counter += 1
+            if p.status is False:
+                p.id = pid
+                p.status = True
+                return counter
+            elif p.id == pid:
+                return -1
+        return 0
+
+    def removePlayer(ctx, pid):
+        for p in Game.players:
+            if p.id == pid:
+                p.reset()
+
+activeGame = Game()
 
 # Error Handling
 # @bot.event
