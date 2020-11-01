0b21497fde6b0a462c4e7b6376d734d24bec9ca8
kevinloeffler <hi@kevinloeffler.ch>
2020-11-01 11:25:25 +0100

    Inital Commit

---
+++ Code/Content.py
@@ -0,0 +1,8 @@
+class Help:
+    howToPlay = """```
+Start by joining a game wit '.join'. Now you see a list of all available colors. Pick one with a reaction. It's resommended that you change your username to your first name making it easier for people to recognice you. Do that by typing '.name <yourname>'.
+```"""
+    commands = """```
+.h          shows this help menu
+.ping       shows the bots current ping in ms
+```"""
